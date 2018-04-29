62ad0eb3ae80b1d4677739b35c338ec8cef339bf
maxisun <00354615@uca.edu.sv>
2018-04-29 16:18:36 -0600

    Initial commit

---
+++ app/src/main/java/com/example/max00/lain/Adapters/RecyclerViewAdapter.java
@@ -0,0 +1,6 @@
+package com.example.max00.lain.Adapters;
+
+import android.support.v7.widget.RecyclerView;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+}
---
+++ app/src/main/java/com/example/max00/lain/Class/Contacto.java
@@ -0,0 +1,31 @@
+package com.example.max00.lain.Class;
+
+import java.io.Serializable;
+
+public class Contacto implements Serializable {
+
+    private String nombre;
+    private int Imagen;
+
+
+    public Contacto(String nombre, int imagen) {
+        this.nombre = nombre;
+        Imagen = imagen;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getImagen() {
+        return Imagen;
+    }
+
+    public void setImagen(int imagen) {
+        Imagen = imagen;
+    }
+}

3518c53daa9eaa4dd425c67c89149c2e49700116
maxisun <00354615@uca.edu.sv>
2018-04-30 00:48:06 -0600

    Se puede leer una imagen desde la galeria

--- app/src/main/java/com/example/max00/lain/Activities/AddContactActivity.java
+++ app/src/main/java/com/example/max00/lain/Activities/AddContactActivity.java
@@ -1,16 +1,27 @@
 package com.example.max00.lain.Activities;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.max00.lain.R;
 
 public class AddContactActivity extends AppCompatActivity {
+
+    private static final int PICK_IMAGE = 100;
+    private Button buttonImagen;
+    private TextView textViewBirthday;
+    private Uri uri;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +29,37 @@ public class AddContactActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_contact);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        imageView = findViewById(R.id.add_img);
+        textViewBirthday = findViewById(R.id.botonBirthday);
+        buttonImagen = findViewById(R.id.botonaddimagen);
+        buttonImagen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+            }
+        });
 
+        textViewBirthday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+            }
+        });
     }
+
+    private void openGallery(){
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery,PICK_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(resultCode == RESULT_OK && requestCode == PICK_IMAGE){
+            uri = data.getData();
+            imageView.setImageURI(uri);
+        }
+    }
+
+
 
 }
